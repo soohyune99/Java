9dfff994375dfc2eb59d7aac8f8b9372afe95a3c
soohyune99 <soohyune1211@gmail.com>
2022-12-26 12:56:38 +0900

    print 출력메소드

---
+++ print/src/printTest/PrintTest1.java
@@ -0,0 +1,16 @@
+package printTest;
+
+public class PrintTest1 {
+
+	public static void main(String[] args) {
+//		주석
+//		1. 소스코드에 설명글을 작성할 때(소스코드 위에 작성)
+//		2. 지금 당장 사용하지 않는 코드를 번역하고 싶지 않을 때 
+		
+		// 이름을 출력하는 부분
+//		System.out.print("홍수현");
+//		나이를 출력하는 부분
+		System.out.print("20살");
+	}
+
+}
---
+++ print/src/printTest/PrintTest2.java
@@ -0,0 +1,18 @@
+package printTest;
+
+public class PrintTest2 {
+	public static void main(String[] args) {
+//		제어 문자
+//		반드시 따옴표 안에서 작성한다.
+		
+//		\n : new line, 줄바꿈, 개행문자
+//		\t	: tab, 위 아래 줄 간격 맞춰 띄기 
+//		\"	: " 표현
+//		\'	: ' 표현
+//		\\	: \표현
+		
+//		System.out.print("홍수현\n");
+		System.out.println("홍수현");
+		System.out.print("\"20살\"");
+	}
+}
